4c2a7d2a63fc549f79eaa46494cc469820bf17b1
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-09-09 17:40:56 -0700

    finish linear/binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for ii, num in enumerate(arr):
+        if num == target:
+            return ii
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
+    low = 0
+    high = len(arr)-1
+    mid = 0
+    while low <= high:
+        mid = low + high //2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
 
     return -1  # not found
